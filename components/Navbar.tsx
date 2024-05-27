4bfd08c8b9ef400e4334c0797e29cc97fb9d6c0d
Yegor Khrapunov <e.hrapunov@npo-echelon.ru>
2024-05-27 19:09:38 +0300

    fix: 42:19

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
+import {NavLinks} from "@/constants";
+import {AuthProviders} from "@/components/AuthProviders";
 
 const Navbar = () => {
+    const session={};
     return (
         <nav className='flexBetween navbar'>
             <div className='flex-1 flexStart gap-10'>
@@ -13,6 +16,30 @@ const Navbar = () => {
                     alt='Flexible'
                     />
                 </Link>
+                <ul className="xl:flex max-sm:hidden text-small gap-7">
+                    {NavLinks.map((link)=>(
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className="flexCenter gap-4">
+                {session
+                    ?
+                    (
+                    <>
+                        UserPhoto
+                        <Link href='/create-project'>
+                            Share Work
+                        </Link>
+                    </>
+                )
+                    :
+                    (
+                        <AuthProviders/>
+                    )
+                }
             </div>
         </nav>
     )
---
+++ grafbase.config.ts
@@ -0,0 +1,32 @@
+import { graph, config } from '@grafbase/sdk'
+
+const g = graph.Standalone();
+
+const User = g.type('User',{
+  name: g.string().length({min:2, max: 20}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.ref('Project').list().optional(),
+});
+
+const Project = g.type('Project',{
+  title: g.string().length({min:3}),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.ref(User),
+})
+
+export default config({
+  graph: g,
+  auth: {
+    rules: (rules) => {
+      rules.public()
+    },
+  },
+})
